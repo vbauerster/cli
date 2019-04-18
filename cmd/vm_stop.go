600056bda479140221acb78d4dae830ed0f052c2
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2019-04-18 12:12:34 +0000

    Make vm stop and start more async
    Signed-off-by: Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>

--- cmd/vm_stop.go
+++ cmd/vm_stop.go
@@ -1,9 +1,8 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
-	"strings"
+	"os"
 
 	"github.com/exoscale/egoscale"
 	"github.com/spf13/cobra"
@@ -11,31 +10,35 @@ import (
 
 // stopCmd represents the stop command
 var vmStopCmd = &cobra.Command{
-	Use:   "stop <vm name> [vm name] ...",
+	Use:   "stop <vm name>+",
 	Short: "Stop virtual machine instance",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
 			return cmd.Usage()
 		}
 
-		errs := []error{}
+		tasks := make([]task, 0, len(args))
 		for _, v := range args {
-			if err := stopVirtualMachine(v); err != nil {
-				errs = append(errs, fmt.Errorf("could not stop %q: %s", v, err))
+			vm, err := getVirtualMachineByNameOrID(v)
+			if err != nil {
+				return err
 			}
+			state := (string)(egoscale.VirtualMachineRunning)
+			if vm.State != state {
+				fmt.Fprintf(os.Stderr, "%q is not in a %s state, got: %s\n", vm.Name, state, vm.State)
+				continue
+			}
+			tasks = append(tasks, task{
+				egoscale.StopVirtualMachine{ID: vm.ID},
+				fmt.Sprintf("Stopping %q ", vm.Name),
+			})
+
 		}
 
-		if len(errs) == 1 {
-			return errs[0]
-		}
-		if len(errs) > 1 {
-			var b strings.Builder
-			for _, err := range errs {
-				if _, e := fmt.Fprintln(&b, err); e != nil {
-					return e
-				}
-			}
-			return errors.New(b.String())
+		taskResponses := asyncTasks(tasks)
+		errors := filterErrors(taskResponses)
+		if len(errors) > 0 {
+			return errors[0]
 		}
 
 		return nil
